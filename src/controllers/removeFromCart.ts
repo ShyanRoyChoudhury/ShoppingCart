ad428d0056836123ed43a980f7b6c56e7c0de284
shayanroychoudhury <shayan.roy31@gmail.com>
2025-02-20 22:29:22 +0530

    add, remove product cart apis

--- src/controllers/removeFromCart.ts
+++ src/controllers/removeFromCart.ts
@@ -5,11 +5,10 @@ import { products } from "../dataStore/products";
 import { carts } from "../dataStore/carts";
 import { AuthenticatedRequest } from "../middleware/auth";
 
-export const addToCart = async (req: AuthenticatedRequest, res: Response) => {
+export const removeFromCart = async (req: AuthenticatedRequest, res: Response) => {
     try{
         const schema = z.object({
             productId: z.number(),  // usually a uuid but number here for easier context understanding
-            cartId: z.number().optional(), // usually a uuid but number here for easier context understanding
         })
 
         const parsedInput = schema.safeParse(req.body);
@@ -17,48 +16,33 @@ export const addToCart = async (req: AuthenticatedRequest, res: Response) => {
             return res.json(new ResponseClass({}, "ERR2", Status.Fail));
         }
 
-        const { productId, cartId } = parsedInput.data;
+        const { productId } = parsedInput.data;
 
 
         const user = req.user;
 
 
-        console.log('userId in api', user)
-
         const product = products.find(p => p.id === productId)
         if(!product) 
             return res.json(new ResponseClass({}, "ERR4", Status.Fail));    // returns product not found
-        if(product?.quantity>0 === false)
-            return res.json(new ResponseClass({}, "ERR5", Status.Fail)) // returns product out of stock
 
         const userSpecificCart = carts.find(c=> c.userId === user.userid)
 
         if(!userSpecificCart){
-            carts.push({
-                userId: user.userid, 
-                cartId: carts.length + 1,
-                products: [{
-                    product,
-                    quantity: 1
-                }],
-                cartOrdered: false
-             })
+            return res.json(new ResponseClass({}, "ERR6", Status.Fail))
         }else{
             const existingProduct = userSpecificCart.products.find(p => p.product.id === productId);
-            if (existingProduct) {                      // Increment quantity if product exists
-                existingProduct.quantity += 1;
-            } else {                                    // Add new product to existing cart
-                userSpecificCart.products.push({
-                    product,
-                    quantity: 1
-                });
+            if (existingProduct) {                      // Decrement quantity if product exists in users cart
+                existingProduct.quantity -= 1;
+            } else {                                    // return if product is not found in users cart
+                return res.json(new ResponseClass({}, "ERR7", Status.Fail))
             }
         }
-        console.log("products", products)
-        product.quantity -= 1;  // decrement product quantity by 1
+
+        product.quantity += 1;  // increment product quantity by 1
         console.log("products", products)
         console.log("carts", carts)
-    return res.json(new ResponseClass({}, "MSG1", Status.Success))
+    return res.json(new ResponseClass({}, "MSG2", Status.Success))
     }catch(err){
         return res.json(new ResponseClass({}, "ERR1", Status.Fail))
     }

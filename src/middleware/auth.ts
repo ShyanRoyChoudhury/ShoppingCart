948775aa21c7f5caafd9d8cb1302ab3327fee363
shayanroychoudhury <shayan.roy31@gmail.com>
2025-02-20 22:32:35 +0530

    update middleware logic

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,18 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import { ResponseClass, Status } from "../utils/ResponseClass";
-import { users } from "../dataStore/user";
+import { users, UserType } from "../dataStore/user";
 
-interface AuthenticatedRequest extends Request {
-    userid?: string;
+export interface AuthenticatedRequest extends Request {
+    user: UserType;
 }
 
 export const verifyAuthMiddleware = async(req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try{
-        const userId = req.body.userId;
-        
-        if(!users.includes(userId)) throw new Error("ERR3")
-        
-        req.userid = userId
+        const userId = req?.body?.userId;
+
+        console.log('userId', userId)
+        if(!userId) throw new Error("Unauthenticated User");
+        const user = users.find(u=> u.userid === userId)
+        if(!user) throw new Error("ERR3");
+        req.user = user
 
         next();
     }catch(err){

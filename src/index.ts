778a22fa9f99450300f99b12e01a3d3924169e0a
shayanroychoudhury <shayan.roy31@gmail.com>
2025-02-21 01:58:37 +0530

    added rate limiter

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,20 @@ const app = express();
 import cartRouter from './routes/cart.routes'
 import checkoutRouter from './routes/checkout.routes';
 import adminRouter from './routes/admin.routes';
+import { rateLimit } from 'express-rate-limit'
 app.use(express.json());
+
+// adding rate limit
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
+
+
+app.use(limiter)
+
 
 app.use('/api/cart', cartRouter)
 app.use('/api/checkout', checkoutRouter)
